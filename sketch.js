eda8076c416f357b0cc7bd0e1122afb192268f58
Joshua Comeau <joshwcomeau@gmail.com>
2018-11-18 08:43:29 -0500

    Create delauney/voronoi sketch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
-const canvasSketch = require('canvas-sketch');
-const { renderPolylines } = require('canvas-sketch-util/penplot');
-const { clipPolylinesToBox } = require('canvas-sketch-util/geometry');
+import canvasSketch from 'canvas-sketch';
+import { renderPolylines } from 'canvas-sketch-util/penplot';
+import { clipPolylinesToBox } from 'canvas-sketch-util/geometry';
+import newArray from 'new-array';
+import triangulate from 'delaunay-triangulate';
+import Voronoi from 'voronoi';
+
+import { random } from './utils';
 
 const settings = {
   dimensions: 'A4',
@@ -10,37 +15,60 @@ const settings = {
   units: 'cm',
 };
 
-const sketch = ({ width, height }) => {
-  // List of polylines for our pen plot
-  let lines = [];
+const convertPointsForVoronoi = points => {
+  return points.map(([x, y]) => ({ x, y }));
+};
 
-  // Draw some circles expanding outward
-  const steps = 5;
-  const count = 20;
-  const spacing = Math.min(width, height) * 0.05;
-  const radius = Math.min(width, height) * 0.25;
-  for (let j = 0; j < count; j++) {
-    const r = radius + j * spacing;
-    const circle = [];
-    for (let i = 0; i < steps; i++) {
-      const t = i / Math.max(1, steps - 1);
-      const angle = Math.PI * 2 * t;
-      circle.push([
-        width / 2 + Math.cos(angle) * r,
-        height / 2 + Math.sin(angle) * r,
-      ]);
-    }
-    lines.push(circle);
-  }
+const sketch = ({ width, height }) => {
+  const pointCount = 200;
+  const margin = 2;
+
+  const positions = Array(pointCount)
+    .fill()
+    .map(() => {
+      // Margin from print edge in centimeters
+      const margin = 2;
+      // Return a random 2D point inset by this margin
+      return [random(margin, width - margin), random(margin, height - margin)];
+    });
+
+  const boundingBox = {
+    xl: 0,
+    xr: width,
+    yt: 0,
+    yb: height,
+  };
+
+  const voronoi = new Voronoi();
+  const voronoiDiagram = voronoi.compute(
+    convertPointsForVoronoi(positions),
+    boundingBox
+  );
+
+  const voronoiPositions = voronoiDiagram.edges
+    .filter(edge => edge.lSite && edge.rSite)
+    .map(edge => [[edge.lSite.x, edge.lSite.y], [edge.rSite.x, edge.rSite.y]]);
+
+  const cells = triangulate(positions);
+
+  let lines = cells.map(cell => {
+    // Get vertices for this cell
+    const triangle = cell.map(i => positions[i]);
+    // Close the path
+    triangle.push(triangle[0]);
+    return triangle;
+  });
+
+  console.log(lines);
 
   // Clip all the lines to a margin
-  const margin = 1.0;
   const box = [margin, margin, width - margin, height - margin];
-  lines = clipPolylinesToBox(lines, box);
+  lines = clipPolylinesToBox(voronoiPositions, box);
 
   // The 'penplot' util includes a utility to render
   // and export both PNG and SVG files
-  return props => renderPolylines(lines, props);
+  console.log([...voronoiPositions, ...lines]);
+  return props => renderPolylines(voronoiPositions, props);
 };
 
 canvasSketch(sketch, settings);
