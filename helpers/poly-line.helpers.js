e26aa9d60de1296fd9b8fbac949760aa5506b1f6
Joshua Comeau <joshwcomeau@gmail.com>
2018-11-21 17:53:50 -0500

    Add NPM script, create plot file

---
+++ helpers/poly-line.helpers.js
@@ -0,0 +1,20 @@
+const { clipPolylinesToBox } = require('canvas-sketch-util/geometry');
+
+export const clipLinesWithMargin = ({
+  margin,
+  width,
+  height,
+  lines,
+  withBorder,
+}) => {
+  // Clip all the lines to a margin
+  const box = [margin, margin, width - margin, height - margin];
+  let newLines = clipPolylinesToBox(lines, box);
+
+  if (withBorder) {
+    newLines = [...newLines, [box[0], box[1]]];
+  }
+
+  console.log(newLines);
+  return newLines;
+};
