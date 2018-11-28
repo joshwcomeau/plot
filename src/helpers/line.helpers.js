2862221f5e752e09ca7d05e32460a7121ad654ab
Joshua Comeau <joshwcomeau@gmail.com>
2018-11-28 09:19:16 -0500

    Start thinking about dashed lines

--- src/helpers/line.helpers.js
+++ src/helpers/line.helpers.js
@@ -1,3 +1,4 @@
+// @flow
 const { clipPolylinesToBox } = require('canvas-sketch-util/geometry');
 const convertUnits = require('convert-units');
 
@@ -18,4 +19,29 @@ export const clipLinesWithMargin = ({
 
   console.log(newLines);
   return newLines;
+};
+
+/**
+ * Given two points, p1 and p2, create an array of tiny dashes that span between
+ * them.
+ *
+ * NOTE: the dashes include the first point, but exclude the last point:
+ *
+ * __   __   __   __   .
+ * ^                   ^
+ * P1                  P2
+ *
+ * This is so that dashed lines can be chained together without doubling the
+ * dash on the connecting point.
+ *
+ * @typedef {[number, number]} Point
+ *
+ * @param {Point} p1 - the start point
+ * @param {Point} p2 - the end point
+ * @param {number} numOfDashes - How many dashes to render
+ * @param {number} lineWidth - how long should each dash be? For a dotted line,
+ *                             supply a really small value.
+ */
+export const createDashedLine = ({ p1, p2, numOfDashes, lineWidth }) => {
+  // TODO
 };
--- src/helpers/line.helpers.test.js
+++ src/helpers/line.helpers.test.js
@@ -1,5 +1,23 @@
-describe('ya', () => {
-  it('doesstuff', () => {
-    expect(true).toBe(true);
+import { createDashedLine } from './line.helpers';
+
+describe('line.helpers', () => {
+  describe('createDashedLine', () => {
+    it('creates a small horizontal 4-dotted line', () => {
+      const p1 = [0, 0];
+      const p2 = [5, 0];
+      const numOfDots = 5;
+      const lineWidth = 0.01;
+
+      const actualOutput = createDashedLine({ p1, p2, numOfDots });
+      const expectedOutput = [
+        [[0, 0], [0.01, 0]],
+        [[1, 0], [1.01, 0]],
+        [[2, 0], [2.01, 0]],
+        [[3, 0], [3.01, 0]],
+        [[4, 0], [4.01, 0]],
+      ];
+
+      expect(actualOutput).toEqual(expectedOutput);
+    });
   });
 });
